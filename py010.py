10b0732ec21ec08926d8f653f3b71c0075bd9805
Bilal Said Yılmaz <146930466+bloodneverlies@users.noreply.github.com>
2024-11-10 15:36:43 +0300

    Create py010.py

---
+++ py010.py
@@ -0,0 +1,18 @@
+# List Comprehensions 
+liste1=[0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20]
+liste2=[sayi for sayi in liste1 if sayi%2==0]
+print(liste1)
+print(liste2)
+
+# List Slicing
+liste3=liste1[::] # Tüm listeyi gezer ve yeni listemize atar 
+print(liste3)
+
+liste3=liste1[3:13:2] # 3 dahil 13 dahil değil 3'ten başlayarak 2'şer adım atlayarak yeni listeye atama yapar
+print(liste3)
+
+liste3=liste1[13:3:-1] # 13 dahil 3 dahil değil 13'ten geriye doğru 1'er adım yeni listemize atama yapar
+print(liste3)
+
+liste3=liste1[1:11:-1] #boş liste döndürür çünkü başlangıçtan bitiş değerine doğru azalarak gidecekken burada başlangıç<bitiş olarak verilmiş
+print(liste3)
