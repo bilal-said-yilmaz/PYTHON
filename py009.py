db31c8af10de967c3372ea646fd761769f84703f
Bilal Said Yılmaz <146930466+bloodneverlies@users.noreply.github.com>
2024-11-10 15:09:34 +0300

    Update py009.py

--- py009.py
+++ py009.py
@@ -1,17 +1,23 @@
 liste1=[1,2,3,4]
 liste2=[1,2,3,4]
 print("liste1 ve liste2 eşit mi?",liste1==liste2)
+print("liste1 > liste2 ?",liste1>liste2)
 
 liste3=[1,2,3,4]
 liste4=[1,2,3,]
 print("liste3 ve liste4 eşit mi?",liste3==liste4)
+print("liste3 > liste4 ?",liste3>liste4)
 
 liste5=[1,2,3,4]
 liste6=[1,2,3,5]
 print("liste5 ve liste6 eşit mi?",liste5==liste6)
+print("liste5 > liste6 ?",liste5>liste6)
 
 liste7=[1,2,3,4]
 liste8=[4,3,2,1]
 print("liste7 ve liste8 eşit mi?",liste7==liste8)
+print("liste7 > liste8 ?",liste7>liste8)
 
 # Buradan şu sonucu rahatlıkla çıkarabiliriz listelerde '==' karşılaştırma operötörü liste uzunluğu ve liste elemanları aynı sırada ve aynı sayıda ise true döner
+# '>' , '<' ifadeleri ise ASCII kodlarına göre karşılaştıma yapar ve index index gider ilk bulduğu farklılıkta true ya da false değer döndürür devamında sonraki indexlere bakmaz
+
