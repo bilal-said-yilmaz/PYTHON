087bf3de96cf0fb39756607045a52e2f653c4440
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-11-29 14:03:24 +0300

    Update py011.py

---
+++ py011.py
@@ -0,0 +1,33 @@
+ogrenciler = []
+alinandeger = 0
+resim = []
+muzik = []
+tiyatro = []
+
+print("8 öğrencinin isimlerini giriniz:")
+for ogrenci in range(8):
+    o = input()
+    ogrenciler.append(o)
+
+for i in range(12):
+    if i < 4:
+        alinandeger = int(input())
+        resim.append(ogrenciler[alinandeger])
+    elif 4 <= i < 8:
+        alinandeger = int(input())
+        muzik.append(ogrenciler[alinandeger])
+    elif 8 <= i < 12:
+        alinandeger = int(input())
+        tiyatro.append(ogrenciler[alinandeger])
+
+ayrikmi = sorted(set(resim).isdisjoint(set(tiyatro)))
+print(ayrikmi)
+
+sadecebiri = sorted(set(resim) ^ set(tiyatro))
+print(sadecebiri)
+
+tumu = sorted(set(resim) | set(tiyatro) | set(muzik))
+print(tumu)
+
+hicbir = sorted(tumu - (set(resim) & set(tiyatro) & set(muzik)))
+print(hicbir)
