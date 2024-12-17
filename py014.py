0104e4170cd21d5cf2cc5146e687f6560d8aaa01
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-17 20:31:19 +0300

    Update py014.py

--- py014.py
+++ py014.py
@@ -4,12 +4,12 @@ class Ogrenci:
         self.__soyadi=soyadi
         self.__numarasi=numarasi
         self.__sinifi=sinifi
-    def sinifGuncelle(self,yeni_sinifi):
+    def sinifGuncelle(self,yeni_sinifi): # methodun adı set_ ile başlasaydı setter methodu olurdu kontrollü güncelleme işlemlerinde kullanılır
         if yeni_sinifi>0:
             self.__sinifi=yeni_sinifi
         else:
             print("Yanlış sınıf girişi")
-    def ogrenciGoster(self):
+    def ogrenciGoster(self): # method adı get_ ile başlasaydı getter methodu olurdu
         return f"Tam adı {self.__adi} {self.__soyadi} olan öğrencimizin numarası: {self.__numarasi}, sınıfı ise: {self.__sinifi} \n"
 
 ogrenci1=Ogrenci("Bilal Said","Yılmaz",11111111111,14)
