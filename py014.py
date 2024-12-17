334aba35952f779fc21eb675b2ed84964d82c6bf
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-17 20:27:02 +0300

    Create py014.py

---
+++ py014.py
@@ -0,0 +1,20 @@
+class Ogrenci:
+    def __init__(self,adi,soyadi,numarasi,sinifi):
+        self.__adi=adi
+        self.__soyadi=soyadi
+        self.__numarasi=numarasi
+        self.__sinifi=sinifi
+    def sinifGuncelle(self,yeni_sinifi):
+        if yeni_sinifi>0:
+            self.__sinifi=yeni_sinifi
+        else:
+            print("Yanlış sınıf girişi")
+    def ogrenciGoster(self):
+        return f"Tam adı {self.__adi} {self.__soyadi} olan öğrencimizin numarası: {self.__numarasi}, sınıfı ise: {self.__sinifi} \n"
+
+ogrenci1=Ogrenci("Bilal Said","Yılmaz",11111111111,14)
+
+print(ogrenci1.ogrenciGoster())
+ogrenci1.sinifGuncelle(15)
+print(ogrenci1.ogrenciGoster())
+ogrenci1.sinifGuncelle(-3)
