43fc0ff4c194b1e64ed11642deab94808f65b349
Bilal Said Yılmaz <146930466+bilal-said-yilmaz@users.noreply.github.com>
2024-12-17 19:45:32 +0300

    Update py012.py

--- py012.py
+++ py012.py
@@ -4,21 +4,21 @@ class BankaHesabi:
         self.turu=turu
         self.bakiye=bakiye
 
-def paraYatir(self,miktar):
-    self.bakiye+=miktar
-    return self.bakiye
+    def paraYatir(self,miktar):
+        self.bakiye+=miktar
+        return self.bakiye
 
-def paraCek(self,miktar):
-    self.bakiye-=miktar
-    return self.bakiye
+    def paraCek(self,miktar):
+        self.bakiye-=miktar
+        return self.bakiye
 
-def bakiyeGoruntule(self):
-    print(f'{self.hesapSahibi} adlı kullanıcının bakiyesi: {self.turu}{self.bakiye}')
+    def bakiyeGoruntule(self):
+        print(f'{self.hesapSahibi} adlı kullanıcının bakiyesi: {self.turu}{self.bakiye}')
 
 bankahesabi1=BankaHesabi("Elon Musk","$",439400000000)
 
-bakiyeGoruntule(bankahesabi1)
-paraYatir(bankahesabi1,100000)
-bakiyeGoruntule(bankahesabi1)
-paraCek(bankahesabi1,2000000)
-bakiyeGoruntule(bankahesabi1)
+BankaHesabi.bakiyeGoruntule(bankahesabi1)
+BankaHesabi.paraYatir(bankahesabi1,100000)
+
+BankaHesabi.paraCek(bankahesabi1,2000000)
+BankaHesabi.bakiyeGoruntule(bankahesabi1)
